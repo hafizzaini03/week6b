18df57ec97d21969a13f8590bd9f1b7fc0610c66
Hafiz Zaini <84973662+hafizzaini03@users.noreply.github.com>
2025-11-19 04:02:43 +0800

    Update lab3b.v

--- lab3b.v
+++ lab3b.v
@@ -1,21 +1,38 @@
-module lab3b (clock50M, reset, d1, d0);
+module lab3b(clock50M, reset, d1, d0);
 input  clock50M, reset;
 output [6:0] d1, d0;
-parameter DIV_COUNT=26'd24_999_999;
+
+parameter DIV_COUNT = 26'd24_999_999;
 
 wire clk_out;
-wire [3:0] count_4bit;
-  wire [25:0] count_div;
+wire [3:0] count_4bit_internal;
+wire [25:0] count_div_internal;
 
-//module clkdvd(clk_in, reset, clk_out, count);
-clkdvd #(DIV_COUNT) cd(clock50M, reset, clk_out, count_div);
+wire [3:0] count_4bit = count_4bit_internal;
+wire [25:0] count_div  = count_div_internal;
 
-//module sucounter4b (clk, rst, q);
-sucounter4b suc(clk_out, reset, count_4bit);
 
-//module lab2a (v, d1, d0);
-lab2a all(count_4bit, d1, d0);
+clkdvd #(DIV_COUNT) cd(
+    .clock_50M(clock50M),
+    .reset(reset),
+    .clk_out(clk_out),
+    .count(count_div_internal)
+);
+
+sucounter4b suc(
+    .clk(clk_out),
+    .reset(reset),
+    .q(count_4bit_internal)
+);
+
+
+lab2a all(
+    .v(count_4bit_internal),
+    .d1(d1),
+    .d0(d0)
+);
 
 endmodule
+
 
 
