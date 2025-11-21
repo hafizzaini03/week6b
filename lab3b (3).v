b80b15e5f9ba571ca527991d9eb470623bff0c89
Hafiz Zaini <84973662+hafizzaini03@users.noreply.github.com>
2025-11-21 19:14:01 +0800

    Update lab3b (3).v

--- lab3b (3).v
+++ lab3b (3).v
@@ -2,22 +2,21 @@ module lab3b(clock50M, reset, d1, d0);
 
 input clock50M;
 input reset;
-output [6:0] d1;
-output [6:0] d0;
+output [6:0] d1, d0;
 
 parameter DIV_COUNT = 26'd24_999_999;
 
 wire slow_clock;
+wire [25:0] count_div;      
 wire [3:0] v, A, m;
 wire gr;
-
 
 clock_divider CD(
     .clock50M(clock50M),
     .reset(reset),
-    .slow_clock(slow_clock)
+    .slow_clock(slow_clock),
+    .count_div(count_div)    
 );
-
 
 counter_4bit C4(
     .clk(slow_clock),
@@ -25,37 +24,18 @@ counter_4bit C4(
     .Q(v)
 );
 
-assign count_4bit = v;    // optional
-
-
-circuit_A a0(
-    .v(v),
-    .A(A)
-);
-
-comparator c0(
-    .v(v),
-    .z(gr)
-);
-
+circuit_A a0(.v(v), .A(A));
+comparator c0(.v(v), .z(gr));
 
 mux21 M0(
     .s(gr),
     .u(v),
-    .v({1'b0, A}),
+    .v(A),        
     .m(m)
 );
 
-
-circuit_b b0(
-    .z(gr),
-    .d1_seg(d1)
-);
-
-
-decoder Dec(
-    .m(m),
-    .seg(d0)
-);
+circuit_b b0(.z(gr), .d1_seg(d1));
+decoder   d0dec(.m(m), .seg(d0));
 
 endmodule
+
