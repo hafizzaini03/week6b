d5b346696e28bc263c15a9cb6564ae8c9199dc18
Hafiz Zaini <84973662+hafizzaini03@users.noreply.github.com>
2025-11-21 19:16:52 +0800

    Update lab3b (3).v

--- lab3b (3).v
+++ lab3b (3).v
@@ -6,31 +6,37 @@ output [6:0] d1, d0;
 
 parameter DIV_COUNT = 26'd24_999_999;
 
+
+wire [3:0] count_4bit;   // testbench needs this
+wire [25:0] count_div;   // testbench also reads this
+
 wire slow_clock;
-wire [25:0] count_div;      
 wire [3:0] v, A, m;
 wire gr;
+
 
 clock_divider CD(
     .clock50M(clock50M),
     .reset(reset),
     .slow_clock(slow_clock),
-    .count_div(count_div)    
+    .count_div(count_div)    //exposed for testbench
 );
+
 
 counter_4bit C4(
     .clk(slow_clock),
     .reset(reset),
-    .Q(v)
+    .Q(count_4bit)           //connect to visible signal
 );
 
-circuit_A a0(.v(v), .A(A));
-comparator c0(.v(v), .z(gr));
+
+circuit_A a0(.v(count_4bit), .A(A));
+comparator c0(.v(count_4bit), .z(gr));
 
 mux21 M0(
     .s(gr),
-    .u(v),
-    .v(A),        
+    .u(count_4bit),
+    .v(A),                   
     .m(m)
 );
 
@@ -38,4 +44,3 @@ circuit_b b0(.z(gr), .d1_seg(d1));
 decoder   d0dec(.m(m), .seg(d0));
 
 endmodule
-
