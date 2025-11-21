badd3f7d50d77e6eae7510f3f54aebf7f600d66f
Hafiz Zaini <84973662+hafizzaini03@users.noreply.github.com>
2025-11-21 19:13:20 +0800

    Update clock_divider.v

--- clock_divider.v
+++ clock_divider.v
@@ -1,24 +1,26 @@
-module clock_divider(clock50M, reset, slow_clock);
+module clock_divider(clock50M, reset, slow_clock, count_div);
 input clock50M, reset;
 output reg slow_clock;
+output reg [25:0] count_div;
 
-reg [25:0] count;
+parameter DIV_COUNT = 26'd24_999_999;
 
-parameter DIV_COUNT=26'd24_999_999; //new clock toggles every 25 million pulses
-
-always@(posedge clock50M or negedge reset)begin
-	if(!reset)begin
-		count <= 0;
-		slow_clock <= 0;
-	end 
-	else begin 
-		if(count == DIV_COUNT) begin
-			count <= 0;
-			slow_clock <= ~slow_clock;
-		end 
-		else begin
-			count <= count + 1;
-		end 
-	end
+always @(posedge clock50M or negedge reset) begin
+    if (!reset) begin
+        count_div <= 0;
+        slow_clock <= 0;
+    end
+    else begin
+        if (count_div == DIV_COUNT) begin
+            count_div <= 0;
+            slow_clock <= ~slow_clock;
+        end
+        else begin
+            count_div <= count_div + 1;
+        end
+    end
 end
+
 endmodule
+
+
